bbb1ede718800be4f691a9a7135dbd58dbd2ed8c
kimjunhyeok18 <1811140059a@ed.fuk.kindai.ac.jp>
2022-11-01 16:50:14 +0900

    add: user name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  helper_method :user_name
   private
 
   def generate_block
@@ -6,5 +7,10 @@ class ApplicationController < ActionController::Base
 
     Glueby::Internal::RPC.client.generatetoaddress(1, @address, ENV['AUTHORITY_KEY'])
     `rails glueby:block_syncer:start`
+  end
+
+  def user_name
+    name = User.find(1).name
+    return name
   end
 end
