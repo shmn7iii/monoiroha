a75dffd3b38777f67db36dcea14bef470f1b6f2c
Akimoto <82797330+motoha0827@users.noreply.github.com>
2022-11-05 02:21:09 +0900

    update: add condition to prevent certain users from being selected for pickup

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   def index
     @items = Item.all
-    @pickup_users = User.order_voted
+    @pickup_users = User.joins(:items).group(:id).having('COUNT(items.id) >= 1').order_voted
   end
 end
