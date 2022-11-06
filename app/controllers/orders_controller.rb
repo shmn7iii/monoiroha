2a24a0343f6de8a931ff4d134f8f2df956fb3541
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2022-11-06 17:42:24 +0900

    refactor: instance variable in controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,23 +1,21 @@
 class OrdersController < ApplicationController
+  before_action :set_item
   after_action :generate_block, only: %i[create]
 
-  def progress
-    @item_id = params[:item_id]
-  end
+  def progress; end
 
   def create
-    item = Item.find(params[:item_id])
     buyer = User.find(1) # XXX: 固定
 
-    item = PurchaseItemService.call(item:, buyer:)
+    PurchaseItemService.call(item: @item, buyer:)
 
     @address = buyer.glueby_wallet.internal_wallet.receive_address
 
     # 本番環境用リダイレクト
     if Rails.env.production?
-      redirect_to "https://monoiroha.shmn7iii.net/orders/complete?item_id=#{item.id}", allow_other_host: true
+      redirect_to "https://monoiroha.shmn7iii.net/orders/complete?item_id=#{@item.id}", allow_other_host: true
     else
-      redirect_to orders_complete_path(item_id: item.id)
+      redirect_to orders_complete_path(item_id: @item.id)
     end
   rescue StandardError
     flash[:danger] = 'Error!'
@@ -29,7 +27,12 @@ class OrdersController < ApplicationController
   end
 
   def complete
-    @item = Item.find(params[:item_id])
     @txid = @item.txid
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -9,17 +9,17 @@ class VotesController < ApplicationController
     voter = User.find(1) # XXX: 固定
     votee = User.find(params[:votee_id])
 
-    vote = VoteUserService.call(votee:, voter:,
-                                vote_token: vote_token = voter.active_vote_token, # XXX: 固定
-                                amount: vote_token&.amount || 0) # XXX: 固定
+    @vote = VoteUserService.call(votee:, voter:,
+                                 vote_token: vote_token = voter.active_vote_token, # XXX: 固定
+                                 amount: vote_token&.amount || 0) # XXX: 固定
 
     @address = voter.glueby_wallet.internal_wallet.receive_address
 
     # 本番環境用リダイレクト
     if Rails.env.production?
-      redirect_to "https://monoiroha.shmn7iii.net/votes/complete?vote_id=#{vote.id}", allow_other_host: true
+      redirect_to "https://monoiroha.shmn7iii.net/votes/complete?vote_id=#{@vote.id}", allow_other_host: true
     else
-      redirect_to votes_complete_path(vote_id: vote.id)
+      redirect_to votes_complete_path(vote_id: @vote.id)
     end
   rescue StandardError
     flash[:danger] = 'Error!'
