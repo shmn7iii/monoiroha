acd645bbdbb9c7b642ad590bbb0c0f7ae928d13d
NosukeSinjo <ayataka3270505@gmail.com>
2022-11-07 08:01:57 +0900

    投票しましたってやつ修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,19 +2,4 @@ class UsersController < ApplicationController
   def index
     @users = User.where.not(id: 1)
   end
-
-  def vote
-    voter = User.find(1) # XXX: 固定
-    user = User.find(params[:id])
-
-    VoteUserService.call(user:,
-                         vote_token: vote_token = voter.active_vote_token, # XXX: 固定
-                         amount: vote_token&.amount || 0) # XXX: 固定
-    @address = voter.glueby_wallet.internal_wallet.receive_address
-    flash[:error] = 'Success!'
-    redirect_to users_path
-  rescue ArgumentError
-    flash[:danger] = 'Error!'
-    redirect_to users_path
-  end
 end
