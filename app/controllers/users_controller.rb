a5af01a2bde2b5599d0e135e6f46bd5a7f3914c6
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2022-12-03 16:51:03 +0900

    update: improve create selection view and demo data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def index
-    @users = User.where.not(id: 1).where.not(id: 999)
+    @users = User.where.not(id: 1).where.not(id: 999).order_voted
+  end
 
   def show
     @user = User.find(params[:id])
